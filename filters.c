0708719235b673a703531d90b18563e9b2924b74
abhinav-dahiya <iit.abhinav.dahiya@gmail.com>
2015-07-24 20:21:21 +0800

    Update filters.c

--- filters.c
+++ filters.c
@@ -1,69 +1,69 @@
 #include "declare.h"
 #include "filters.h"
 
-float discrete_diff(float xi, double dT, int freq)
+float discrete_diff(float xi, double dT, float freq)
 {
-	static double x_old[2];
-	static double dx_old[2];
-	double tau = 1/(2*3.14159*freq);
+	static double x_old[2] = {0.0,0.0};
+	static double dx_old[2] = {0.0,0.0};
+	double tau = 1.0/(2.0*3.14159*freq);
 
-	double A = 2.0*dT/pow((2.0*tau + dT),2);
-	double B = -1 * A;
-	double C = 2.0*(2.0*tau - dT)/(2.0*tau +dT);
-	double D = -1 * pow(((2.0*tau - dT)/(2.0*tau + dT)),2);
+	double A = 2.0*(dT/(pow((2.0*tau + dT),2.0)));
+	double B = -1.0 * A;
+	double C = (2.0*(2.0*tau - dT))/(2.0*tau +dT);
+	double D = -1.0 * (pow(((2.0*tau - dT)/(2.0*tau + dT)),2.0));
 	static float itr = 1;
 
-	dx = A*xi + B*x_old[1] + C*dx_old[0] + D*dx_old[1];
+	float derivative = A*xi + B*x_old[1] + C*dx_old[0] + D*dx_old[1];
 
 	if(itr < 2.5)
 	{
-		dx = (xi - x_old[0])/dT;
+		derivative = (xi - x_old[0])/dT;
 		++itr;
 	}
 
 	dx_old[1] = dx_old[0];
-	dx_old[0] = dx;
+	dx_old[0] = derivative;
 
 	x_old[1] = x_old[0];
 	x_old[0] = xi;
 
-	printf("Diff. Done. \n");
-	return dx;
+	printf("Diff. Done. and dx = %f\n", derivative);			 
+	return derivative;
 }
 
 
 
-float low_pass_filter(float xi, double dT, int freq)
+float low_pass_filter(float xi, double dT, float freq)
 {
-	static float x_old[2];
-	static float xf_old[2];
+	static float x_old[2] = {0.0,0.0};
+	static float xf_old[2] = {0.0,0.0};
 
-	double a = 2 * 3.14159 * freq;
-	double p = 2.0/dT;
+	float a = 2.0 * 3.14159 * freq;
+	float p = 2.0/dT;
 
-	double A = pow((a/(a+p)),2);
-	double B = 2 * A;
-	double C = A;
-	double D = -2 * (a-p) / (a+p);
-	double E = -1 * pow(((a-p)/(a+p)),2);
+	float A = pow((a/(a+p)),2.0);
+	float B = 2.0 * A;
+	float C = A;
+	float D = -2.0 * (a-p) / (a+p);
+	float E = -1.0 * pow(((a-p)/(a+p)),2.0);
 	static float itr = 1;
 
-	xf = A*xi + B*x_old[0] + C*x_old[1] + D*xf_old[0] + E*xf_old[1];
+	float filtered = (A*xi) + B*x_old[0] + C*x_old[1] + D*xf_old[0] + E*xf_old[1];
 
 	if(itr < 2.5)
 	{
-		xf = xi;
+		filtered = xi;
 		++itr;
 	}
 
 	xf_old[1] = xf_old[0];
-	xf_old[0] = xf;
+	xf_old[0] = filtered;
 
 	x_old[1]  = x_old[0];
 	x_old[0]  = xi;
 
-	printf("iltering done.\n");
-	return xf;
+	printf("Filtering done and xf = %f\n", filtered);
+	return filtered;
 }
 
 
@@ -73,7 +73,7 @@ float discrete_intg(float pow, double dT)
 	static float E_old;
 
 	float A = dT/2.0;
-	static float itr = 1;
+	static float itr = 1.0;
 
 	float E = E_old + A*(pow + pow_old);
 
