47ca77ebe0722f2d993e7fe71cb8903ad92e8df8
abhinav-dahiya <iit.abhinav.dahiya@gmail.com>
2015-07-27 20:29:53 +0800

    Update calculate_I_ref.c

--- calculate_I_ref.c
+++ calculate_I_ref.c
@@ -1,19 +1,34 @@
+#include "declare.h"    
 #include "calculate_I_ref.h"
-#include "declare.h"
 
 
-float calculate_I_ref(void)
+void calculate_I_ref(void)
 {	
-	printf("I_ref\n");
+	while(1)
+	{
+		if(encoder_flag) // encoder flag = 1 means xf and dx are calculated
+		{
+			//calculate I_ref and convert to pwm value
+			printf("I_ref: kp and kd are: %f, %f\n", kp, kd);
+		I_ref = (float)((kp)*(xd - xf)) - (kd)*(dx);
+		//	I_range = 2;     // for range -2 to +2, set during motor controller setup
+		
+		if(I_ref > I_range)
+			I_ref = I_range;
+		if(I_ref < (-1.0*I_range))
+			I_ref = (-1.0*I_range);
 	
-	//calculate I_ref and convert to pwm value
-	I_ref = (kp)*(xd - xf) - (kd)*(dx);
-	I_range = 2;     // for range -2 to +2
-	float pwm_value = (I_ref + I_range) * (512 / I_range); // I_range = range set suring motor controller setup
+		// maxon controller requires PWM value to be 10% and 90%
+		// so, I_ref should be scaled beetween 103 and 921 (10% and 90% of 1024)
 	
-	//reset flags
-	encoder_flag = 0;
+		float pwm_value = (204.0 * I_ref) + 512.0;
+		
+		bcm2835_pwm_set_data(PWM_CHANNEL, pwm_value);
 	
-	return pwm_value;
-	
+//		printf("Xf = %f\n", xf);
+		//reset flag
+		encoder_flag = 0;
+		}
+	}	
+
 }
